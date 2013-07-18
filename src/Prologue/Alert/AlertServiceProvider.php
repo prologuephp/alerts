87b6dea142b1dd9239ff83f0c7dcf355449aa9cd
Dries Vints <dries.vints@gmail.com>
2013-07-18 21:51:59 +0200

    Initial commit
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ src/Prologue/Alert/AlertServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Prologue\Alert;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlertServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
